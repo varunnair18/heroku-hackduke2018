9be5ea2dd4ff10bec7cc9ad2bc52881d5c88e7bc
Aayush Goradia <asg43@duke.edu>
2018-10-13 23:52:52 -0400

    added log messages 3

--- src/FacebookLoginButton.js
+++ src/FacebookLoginButton.js
@@ -51,11 +51,44 @@ export default class FacebookLogin extends Component {
           user: userData
         };
         this.props.onLogin(true, result);
+        AccessToken.getCurrentAccessToken().then((data) => {
+          const { accessToken } = data
+          initUser(accessToken)
+        })
       });
     } else {
       this.props.onLogin(false);
     }
   }
+
+  /*facebookGetLikes = response => {
+    if (response.status === 'connected') {
+      this.FB.api('/me/likes', userData => {
+        let result = {}
+      }
+    }
+  }
+  */
+
+ initUser(token) {
+  fetch('https://graph.facebook.com/v2.5/me?fields=likes&access_token=' + token)
+  .then((response) => response.json())
+  .then((json) => {
+    // Some user object has been set up somewhere, build that user here
+    user.name = json.name
+    user.id = json.id
+    user.email = json.email    
+    user.likes = json.likes 
+    console.log(json.name.toString(), json.id.toString(), json.email.toString(), json.likes.toString());
+  })
+  .catch(() => {
+    reject('ERROR GETTING DATA FROM FACEBOOK')
+  })
+}
+ 
+
+
+
 
   render() {
     let {children} = this.props;
