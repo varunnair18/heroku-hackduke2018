a21a84795b6f29d5b4806a9a5893791dc22e603b
Aayush Goradia <asg43@duke.edu>
2018-10-14 11:48:12 -0400

    day2

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import FacebookLoginButton from './FacebookLoginButton.js';
+import Fbutton from './fbutton.js';
+import Charity from './charity.js';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.onFacebookLogin.bind(this);
-  }
-
-  state = {
-    username: null
-  };
-
-
-
-  onFacebookLogin = (loginStatus, resultObject) => {
-    console.log('in onFacebookLogin');
-    if (loginStatus === true) {
-      console.log('login Status is true')
-      this.setState({
-        username: resultObject.user.name
-      });
-    } else {
-      //alert('Facebook login error');
-      console.log("else");
-    }
-  }
-  render() {
-    const { username } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">React Social Media Login</h1>
-        </header>
-
-        <div className="App-intro">
-          { !username &&
-            <div>
-              <p>Click on one of any button below to login</p>
-              <FacebookLoginButton onLogin={this.onFacebookLogin}>
-                <button> Facebook</button>
-              </FacebookLoginButton>
-            </div>
-          }
-          {username &&
-            <p>Welcome back, {username}</p>
-          }
-        </div>
-      </div>
-    );
-  }
+	constructor(){
+		super();
+	}
+	render() {
+		return(
+			<div>
+			<Fbutton/>
+			<Charity/>
+			</div>
+		);
+	}
 }
-
 export default App;
---
+++ src/charity.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const API = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=13c66db4&app_key=443d1b17b656804edeef8d1dbdd40b67';
+const rated = '&rated=true';
+const pageSize = '&pageSize=1';
+const sort = '&sort=RATING%3ADESC';
+
+const donorPrivacy = '&donorPrivacy=';
+const stat = '&state=';
+const searchy = '&search=';
+
+const search = ['alzheimer', 'dog', 'orphan', 'daddy','firefighter'];
+
+class Charity extends Component {
+    
+  constructor(props) {
+    super(props);
+
+      this.state = {
+	  
+	  donorPriv : '1',
+	  stat : '',
+	  url: [],
+	  name: [],
+	  tagLine:[],
+	  mission:[],
+      };
+  }
+
+    componentDidMount() {
+	
+	{/*
+	   must have set donorPriv and stat at this point 
+	 */}
+	
+	var ur = API+rated+sort+pageSize
+	    +donorPrivacy+this.state.donorPriv
+	    +stat+this.state.stat
+	
+	for (var i = 0; i < search.length; i++) {
+	    
+	    fetch(ur + searchy +search[i])
+		.then(response => response.json())
+		.then(response => response[0]) 
+		.then(data => this.setState({
+		    url: this.state.url.concat([data.websiteURL]),
+		    name: this.state.name.concat([data.charityName]),
+		    tagLine: this.state.tagLine.concat([data.tagLine]),
+		    mission: this.state.mission.concat([data.mission])}));
+
+	}
+
+    }
+
+      render() {
+	  
+	  var lis = [];
+	  for (var i = 0; i < search.length; i++) {
+	      lis.push(
+		  		  <div>
+	      <h1> {this.state.name[i]} </h1>
+	      <h1> <a href={this.state.url[i]}> Website </a> </h1>
+	      <h1> {this.state.tagLine[i]} </h1> <br/>
+		      </div>
+	      )
+	  }
+	  
+	  return (lis);
+
+  }
+}
+
+export default Charity;
