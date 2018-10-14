4fd377b60a59a7ddd472326075eeb91c5f2afdc5
Aayush Goradia <asg43@duke.edu>
2018-10-14 01:30:33 -0400

    added log messages 16

--- src/FacebookLoginButton.js
+++ src/FacebookLoginButton.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GraphRequest from 'react-native-fbsdk';
 
 export default class FacebookLogin extends Component {
 
@@ -51,10 +52,16 @@ export default class FacebookLogin extends Component {
           user: userData
         };
         this.props.onLogin(true, result);
-        AccessToken.getCurrentAccessToken().then((data) => {
-          const { accessToken } = data
-          initUser(accessToken)
-        })
+        let req = new GraphRequest('/me', {
+          httpMethod: 'GET',
+          version: 'v2.5',
+          parameters: {
+              'fields': {
+                  'string' : 'email,name,friends'
+              }
+          }
+        });
+        console.log(req.result)
       });
     } else {
       this.props.onLogin(false);
@@ -70,26 +77,7 @@ export default class FacebookLogin extends Component {
   }
   */
 
- initUser(token) {
-  fetch('https://graph.facebook.com/v2.5/me?fields=likes&access_token=' + token)
-  .then((response) => response.json())
-  .then((json) => {
-    // Some user object has been set up somewhere, build that user here
-    user.name = json.name
-    user.id = json.id
-    user.email = json.email    
-    user.likes = json.likes 
-    console.log(json.name.toString(), json.id.toString(), json.email.toString(), json.likes.toString());
-  })
-  .catch(() => {
-    reject('ERROR GETTING DATA FROM FACEBOOK')
-  })
-}
  
-
-
-
-
   render() {
     let {children} = this.props;
     return (
